c73169d7bfee72b68141c2d8c9a859bc767bb49b
矢吹 幸翔 <241323@st.yoshida-g.ac.jp>
2025-03-05 17:21:05 +0900

    コメント直し(途中)

--- main/camera.h
+++ main/camera.h
@@ -1,30 +1,37 @@
-#pragma once
+//=============================================================================
+//
+//	ブロック処理 [block.h]
+// Author : 佐々木奏一郎
+//
+//=============================================================================
 #ifndef _CAMERA_H_
 #define _CAMERA_H_
 
 #include "main.h"
+
+// マクロ定義
 #define MAX_CAMERA (2)
 
-//構造体
+// 構造体
 typedef struct
 {
-	D3DXVECTOR3 posV;			//	視点
-	D3DXVECTOR3 posR;			//	注視点
-	D3DXVECTOR3 vecU;			//	上方向ベクトル
-	D3DXMATRIX mtxProjection;	//	プロジェクションマトリックス
-	D3DXMATRIX mtxView;			//	ビューマトリックス
-	D3DXVECTOR3 rot;			//	向き
-	float fDistance;			//	視点から注視点の距離
-	D3DXVECTOR3 posRDest;
-	D3DXVECTOR3 posVDest;
-	D3DVIEWPORT9 viewport;
-	float deltaX;
-	float deltaY;
-	POINT cursorPos;
-	POINT prevCursorPos;
+	D3DXVECTOR3 posV;			// 視点
+	D3DXVECTOR3 posR;			// 注視点
+	D3DXVECTOR3 vecU;			// 上方向ベクトル
+	D3DXMATRIX mtxProjection;	// プロジェクションマトリックス
+	D3DXMATRIX mtxView;			// ビューマトリックス
+	D3DXVECTOR3 rot;			// 向き
+	float fDistance;			// 視点から注視点の距離
+	D3DXVECTOR3 posRDest;		// 目的の位置(右)
+	D3DXVECTOR3 posVDest;		// 目的の位置(左)
+	D3DVIEWPORT9 viewport;		// ビューポート
+	float deltaX;				// デルタX
+	float deltaY;				// デルタY
+	POINT cursorPos;			// マウスカーソルの位置
+	POINT prevCursorPos;		// カーソルの見える位置
 }Camera;
 
-//	ぷろとたいぷ
+// ぷろとたいぷ
 void InitCamera(void);
 void UninitCamera(void);
 void UpdateCamera(void);
