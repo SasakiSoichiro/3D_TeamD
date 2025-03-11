6bdd54deb7a2590ea4ef009c516e1f485f1d28f9
矢吹 幸翔 <241323@st.yoshida-g.ac.jp>
2025-03-11 17:25:44 +0900

    TTTTTTTTTTTT

--- main/enemy.h
+++ main/enemy.h
@@ -6,8 +6,11 @@
 //=============================================================================
 #ifndef _ENEMY_H_//このマクロ定義がされていなかったら
 #define _ENEMY_H_//2銃インクルード防止のマクロ定義
+
 #include "main.h"
 #include "model.h"
+
+// マクロ定義
 #define GRAVI (0.05f)			// 重力
 #define JUMP (2.0f)
 #define OBJ_E (20.0f)
@@ -19,13 +22,13 @@
 //敵の状態
 typedef enum
 {
-	ENEMYSTATE_NORMAL = 0,	// 徘徊
-	ENEMYSTATE_CHASE,		// 追いかける
-	ENEMYSTATE_DAMAGE,		// スタン
-	ENEMYSTATE_ACTION,		// 攻撃
+	ENEMYSTATE_NORMAL = 0,		// 徘徊
+	ENEMYSTATE_CHASE,			// 追いかける
+	ENEMYSTATE_DAMAGE,			// スタン
+	ENEMYSTATE_ACTION,			// 攻撃
 	ENEMYSTATE_MAX
-}ENEMYSTATE;
 
+}ENEMYSTATE;
 
 //モーションの種類
 typedef enum
@@ -36,33 +39,35 @@ typedef enum
 	EMOTIONTYPE_JUMP,			// ジャンプ
 	EMOTIONTYPE_LANDING,		// 着地
 	EMOTIONTYPE_MAX
+
 }EMOTIONTYPE;
 
 //キーの構造体
 typedef struct
 {
-	float fPosX;//位置
+	float fPosX;				// 位置
 	float fPosY;
 	float fPosZ;
-	float fRotX;//向き
+	float fRotX;				// 向き
 	float fRotY;
 	float fRotZ;
+
 }EKEY;
 
 //キー情報の構造体
 typedef struct
 {
-	int nFlame;//再生フレーム
-	EKEY aKey[EPARTS_MAX];//パーツの最大数を入れる//キー情報
+	int nFlame;					// 再生フレーム
+	EKEY aKey[EPARTS_MAX];		// パーツの最大数を入れる//キー情報
 
 }EKEY_INFO;
 
 //モーション情報の構造体
 typedef struct
 {
-	bool bLoop;//ループするかどうか
-	int nNumKey;//キーの総数
-	EKEY_INFO aKeyInfo[10];//キーの最大数//キー情報
+	bool bLoop;					// ループするかどうか
+	int nNumKey;				// キーの総数
+	EKEY_INFO aKeyInfo[10];		// キーの最大数//キー情報
 }EMOTION_INFO;
 
 //プレイヤー構造体
@@ -74,8 +79,8 @@ typedef struct
 	D3DXVECTOR3 move;
 	D3DXVECTOR3 rot;
 	D3DXVECTOR3 rotDest;
-	D3DXVECTOR3 posVec[2];		//視野用ベクトル
-	D3DXMATRIX mtxWorld;		//ワールドマトリックス
+	D3DXVECTOR3 posVec[2];		// 視野用ベクトル
+	D3DXMATRIX mtxWorld;		// ワールドマトリックス
 	int nLife;
 	int nType;
 	int State;
@@ -83,28 +88,28 @@ typedef struct
 	float fRad;
 	bool bUse;
 
-	Model aModel[EPARTS_MAX];	//モデルのパーツ数
-	int nNumModel;		//モデルのパーツ総数
+	Model aModel[EPARTS_MAX];	// モデルのパーツ数
+	int nNumModel;				// モデルのパーツ総数
 
-	EMOTION_INFO aMotionInfo[8];//モーションの最大数
-	int nNumMotion;//モーションの総数
-	EMOTIONTYPE motionType;//モーションの種類
-	bool bLoopMotion;//ループするかどうか
-	int nNumKey;//キーの総数
-	int nKey;//現在のキーNo.
-	int nCntMotion;//モーションカウンター
+	EMOTION_INFO aMotionInfo[8];// モーションの最大数
+	int nNumMotion;				// モーションの総数
+	EMOTIONTYPE motionType;		// モーションの種類
+	bool bLoopMotion;			// ループするかどうか
+	int nNumKey;				// キーの総数
+	int nKey;					// 現在のキーNo.
+	int nCntMotion;				// モーションカウンター
 
-	bool bFinishMotion;			//現在のモーションが終了しているかどうか
-	bool bBlendMotion;			//ブレンドモーションがあるかどうか
-	EMOTIONTYPE motionTypeBlend;//モーションの種類
-	int nNumKeyBlend;			//キーの総数
-	int nKeyBlend;				//現在のキーNo.
-	int nCntMotionBlend;		//モーションカウンター
-	int nFrameBlend;			//ブレンドのフレーム数
-	int nCntBlend;				//ブレンドカウンター
+	bool bFinishMotion;			// 現在のモーションが終了しているかどうか
+	bool bBlendMotion;			// ブレンドモーションがあるかどうか
+	EMOTIONTYPE motionTypeBlend;// モーションの種類
+	int nNumKeyBlend;			// キーの総数
+	int nKeyBlend;				// 現在のキーNo.
+	int nCntMotionBlend;		// モーションカウンター
+	int nFrameBlend;			// ブレンドのフレーム数
+	int nCntBlend;				// ブレンドカウンター
 
 
-	int nIdxLife;//ライフバーのIdx
+	int nIdxLife;				// ライフバーのIdx
 	int IdxShadow;
 }Enemy;
 
@@ -124,6 +129,7 @@ int GetNumEnemy();
 bool IsRange();
 void SetMotionType(EMOTIONTYPE MotionType, bool bBlendMotion, int nFrameBlend,int nCnt);
 void SetEnemyDistance(float fDistance);
+
 static const char* EPARTS_FILE[EPARTS_MAX] =
 {
 	"data\\model\\redcar000.x",//胴
