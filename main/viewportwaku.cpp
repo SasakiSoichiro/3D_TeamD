a3cc872bbb9dd4c3ae994539bc1f33cc04812822
SasakiSoichiro <241307@yoshida-g.ac.jp>
2025-03-18 17:56:03 +0900

    x

--- main/viewportwaku.cpp
+++ main/viewportwaku.cpp
@@ -26,7 +26,7 @@ void InitViewUI(void)
 
 	//テクスチャの読み込み
 	D3DXCreateTextureFromFile(pDevice,
-		"data\\texture\\enemywaku1.png",
+		"data\\texture\\viewportFrame.png",
 		&g_pTextureViewUI);
 
 	pDevice->CreateVertexBuffer(sizeof(VERTEX_2D) * 4,//(sizeof(VERTEX_2D)*必要な頂点数
@@ -41,10 +41,10 @@ void InitViewUI(void)
 	g_pVtxBuffViewUI->Lock(0, 0, (void**)&pVtx, 0);
 
 	//頂点座標の設定
-	pVtx[0].pos = D3DXVECTOR3(417.5f, -20.0f, 0.0f);
-	pVtx[1].pos = D3DXVECTOR3(836.0f, -20.0f, 0.0f);
-	pVtx[2].pos = D3DXVECTOR3(417.5f, 227.0f, 0.0f);
-	pVtx[3].pos = D3DXVECTOR3(836.0f, 227.0f, 0.0f);
+	pVtx[0].pos = D3DXVECTOR3(430.0f, -20.0f, 0.0f);
+	pVtx[1].pos = D3DXVECTOR3(820.0f, -20.0f, 0.0f);
+	pVtx[2].pos = D3DXVECTOR3(430.0f, 220.0f, 0.0f);
+	pVtx[3].pos = D3DXVECTOR3(820.0f, 220.0f, 0.0f);
 
 	//rhwの設定
 	pVtx[0].rhw = 1.0f;
@@ -53,10 +53,10 @@ void InitViewUI(void)
 	pVtx[3].rhw = 1.0f;
 
 	//頂点カラーの設定　　　　
-	pVtx[0].col = D3DXCOLOR(1.0f, 1.0f, 1.0f, 1.0f);
-	pVtx[1].col = D3DXCOLOR(1.0f, 1.0f, 1.0f, 1.0f);
-	pVtx[2].col = D3DXCOLOR(1.0f, 1.0f, 1.0f, 1.0f);
-	pVtx[3].col = D3DXCOLOR(1.0f, 1.0f, 1.0f, 1.0f);
+	pVtx[0].col = D3DXCOLOR(0.0f,0.0f, 0.0f, 1.0f);
+	pVtx[1].col = D3DXCOLOR(0.0f,0.0f, 0.0f, 1.0f);
+	pVtx[2].col = D3DXCOLOR(0.0f,0.0f, 0.0f, 1.0f);
+	pVtx[3].col = D3DXCOLOR(0.0f,0.0f, 0.0f, 1.0f);
 
 	//テクスチャ座標の設定
 	pVtx[0].tex = D3DXVECTOR2(0.0f, 0.0f);
